b5df319d5e062662f0169dfeb2411e097e12e153
abdulmoizsoomrocs <abdulmoizsoomrocs@gmail.com>
2026-04-19 02:11:43 +0500

    fix port issue

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,13 @@ app.use(express.json());
 
 // ✅ VERY IMPORTANT LINE
 app.use("/api/products", productRoutes);
-
+console.log("Routes loaded");
 // test route (add this)
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
