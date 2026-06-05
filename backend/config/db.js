f855451741e381195b7ba6a0c06d374cfb3b81c3
abdulmoizsoomrocs <abdulmoizsoomrocs@gmail.com>
2026-06-06 02:07:30 +0500

    chagne the database name

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected DB:", conn.connection.name);
+console.log("MongoDB Connected:", conn.connection.host);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
